554c550ff0ad33d32d258edc70a7026a84d23ef6
Cristian Ionel <cristian.ionel@gmail.com>
2016-09-07 00:04:13 +0200

    Import SEO Titles & Desc

--- functions.php
+++ functions.php
@@ -17,7 +17,6 @@ $sage_includes = [
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php', // Theme customizer
   'lib/wp_bootstrap_navwalker.php', // Bootstrap Walker
-//  'lib/bootstrap_ninjaforms.php', // Bootstrap Ninja Forms
 ];
 
 foreach ($sage_includes as $file) {
@@ -28,3 +27,31 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+/* Programatically import all previous SEO Title and Descriptions to YOAST */
+function convert_seo_meta() {
+
+  $all_pages = get_posts(array(
+    'posts_per_page'   => -1,
+    "post_type" => array("post","page")
+  ));
+
+  foreach ($all_pages as $single_page) {
+
+    $post_id = $single_page->ID;
+
+    $uxi_seo_title = get_post_meta($post_id, "uxi_seo_title", true);
+    $uxi_seo_description = get_post_meta($post_id, "uxi_seo_description", true);
+    update_post_meta($post_id, "_yoast_wpseo_title", $uxi_seo_title);
+    update_post_meta($post_id, "_yoast_wpseo_metadesc", $uxi_seo_description);
+    update_post_meta($post_id, "_yoast_wpseo_focuskw_text_input", "Dentist Lady Lake");
+    update_post_meta($post_id, "_yoast_wpseo_focuskw", "Dentist Lady Lake");
+
+  }
+
+}
+
+if(isset($_GET['import_seo']) && $_GET['import_seo'] == "true"){
+  convert_seo_meta();
+}
